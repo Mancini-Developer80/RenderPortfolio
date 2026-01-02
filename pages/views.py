2c29edd7f696c76d45c8053955a1115738cc61f5
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2026-01-02 14:40:30 +0100

    Django structure improved

--- pages/views.py
+++ pages/views.py
@@ -1,31 +1,31 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request, 'index.html')
+	return render(request, 'pages/home.html')
 
 def projects(request):
-	return render(request, 'projects.html')
+	return render(request, 'pages/projects.html')
 
 def about(request):
-	return render(request, 'about.html')
+	return render(request, 'pages/about.html')
 
 def contact(request):
-	return render(request, 'contact.html')
+	return render(request, 'pages/contact.html')
 
 def angular_myflix_case(request):
-	return render(request, 'angularmyflixcase.html')
+	return render(request, 'pages/cases/angular-myflix.html')
 
 def meetapp_case(request):
-	return render(request, 'meetappcase.html')
+	return render(request, 'pages/cases/meetapp.html')
 
 def pokemon_case(request):
-	return render(request, 'pokemoncase.html')
+	return render(request, 'pages/cases/pokemon.html')
 
 def promontolio_case(request):
-	return render(request, 'promontoliocase.html')
+	return render(request, 'pages/cases/promontolio.html')
 
 def myflix_case(request):
-	return render(request, 'myflixcase.html')
+	return render(request, 'pages/cases/myflix.html')
 
 def jobtracker_case(request):
-	return render(request, 'jobtracker.html')
+	return render(request, 'pages/cases/jobtracker.html')
