dadc06b9f0b7b5d5a72a23190faafa9971dd8596
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2026-01-07 14:01:16 +0100

    portfolio settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,13 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pdl)16usk0&i$xaq&&j6wsrg8^dq@xf%#6#+c$igviph+a5(*)'
+#SE CURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["giuseppemancini.dev",
+                 "www.giuseppemancini.dev", 
+                 "giuseppemancini.onrender.com"
+                 ]
 
 
 # Application definition
